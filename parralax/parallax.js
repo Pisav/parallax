2201cd556f266e2e382452bf18b4b59f608b17c0
Vadim Pisa <27266@ma-web.nl>
2019-10-29 11:59:28 +0100

    Update parallax.js

--- parralax/parallax.js
+++ parralax/parallax.js
@@ -4,14 +4,19 @@ let sectie4 = document.getElementsByClassName('sectie--4')[0];
 
 window.addEventListener('scroll', (e)=> {
 	scrollAfstand = window.pageYOffset;
-
+	// console.log('scrollAfstand: '+scrollAfstand);
 	corrSection2(scrollAfstand);
 	corrSection4(scrollAfstand);
 	corrSection6(scrollAfstand);
 });
 
 const corrSection2 = (gescrolled) => {
-	sectie2.style.backgroundPositionY =  -gescrolled/8 + "px";
+	// console.log('In sectie 2: gescrolled: '+gescrolled);
+	let correctie = gescrolled/-4;
+	correctie -= 200;
+	correctie += 'px';
+	console.log("correctie: " +  correctie);
+	sectie2.style.backgroundPositionY = correctie;
 }
 const corrSection4 = (gescrolled) => {
 	sectie2.style.backgroundPositionY = -gescrolled/3 + "px";
